f310eb5c41a7092207499ba1cf1c0dd46e9547b3
kambaikariuki <kariukikambai@gmail.com>
2025-09-08 17:33:50 +0300

    Task 5 done

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry of program
+*
+* Description: prints statemement to screen
+*
+* Return: Returns 0 on success
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
