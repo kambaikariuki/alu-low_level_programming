6a50d130c0e9532f63dd9036d3e259de38328c9a
kambaikariuki <kariukikambai@gmail.com>
2025-09-11 15:14:02 +0300

    Task 0 done

---
+++ variables_if_else_while/0-postive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry of the program
+ *
+ * Description: assigns a rand num to n
+ * prints if its positive or negative
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is positive\n", n);
+}
+return (0);
+}
