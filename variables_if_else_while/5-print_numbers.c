49c7e9f6d3cd2d82eed9866ea736a3804542dce2
kambaikariuki <kariukikambai@gmail.com>
2025-09-14 17:01:08 +0300

    Task 5 done

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry of the program
+*
+* Description: prints all single digit numbers in base 10
+*
+* Return: 0 on success
+*
+*/
+int main(void)
+{
+char num = '0', newline = '\n';
+for (num; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar(newline);
+return (0);
+}
