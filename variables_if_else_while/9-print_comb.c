eb207362d4a6202f57d6d5637a00e3bd598ed759
kambaikariuki <kariukikambai@gmail.com>
2025-09-14 17:39:35 +0300

    Task 9 done

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry of the program
+*
+* Description: prints all single digit numbers in base 10
+*
+* Return: 0 on success
+*
+*/
+int main(void)
+{
+int num = 0;
+for (; num < 10; num++)
+{
+putchar('0' + num);
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
