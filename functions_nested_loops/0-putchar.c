67a8c7194349fcf3019d394eb931ef507f5e53d3
kambaikariuki <kariukikambai@gmail.com>
2025-09-22 22:37:41 +0300

    Task 0 done

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry of program
+*
+* Description: prints _putchar followed by a new line
+*
+* Return: 0 on success
+*
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
